4b41839ed10ba53f94f09b6dd8ec16c7395f13b7
kod2nd <colinongengkiat@gmail.com>
2018-07-25 14:34:20 +0800

    fetched books data

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const logger = require("morgan");
+const cors = require('cors')
 
 const index = require("./routes/index");
 const books = require("./routes/books.js");
 const authors = require("./routes/authors.js");
 
+var whitelist = ['http://localhost:3000/', 'http://example2.com']
+var corsOptions = {
+    origin: whitelist
+}
+
 const app = express();
 app.use(logger("dev"));
 app.use(express.json());
+app.use(cors(corsOptions))
 
 app.use("/", index);
 app.use("/books", books);
